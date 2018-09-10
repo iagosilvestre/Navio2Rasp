e43cf8047bc274d768f6a432ac8966a20bcf9085
iago silvestre <iagosilvestrehp@hotmail.com>
2018-09-10 14:48:14 -0300

    spacing format

--- C++/Examples/teste/teste.cpp
+++ C++/Examples/teste/teste.cpp
@@ -131,7 +131,7 @@ int main(int argc, char *argv[])
 
         barometer.calculatePressureAndTemperature();
 
-        printf("Temperature(C): %f Pressure(millibar): %f\n", 
+        printf("Temperature(C): %f Pressure(millibar): %f\n\n\",
                 barometer.getTemperature(), barometer.getPressure());
                 
 
