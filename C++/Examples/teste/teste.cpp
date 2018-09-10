7e2e0eff6c27c5653c885dfae72c2addc285da87
iago silvestre <iagosilvestrehp@hotmail.com>
2018-09-10 15:56:33 -0300

    included gps

--- C++/Examples/teste/teste.cpp
+++ C++/Examples/teste/teste.cpp
@@ -16,6 +16,8 @@ Sensors names: mpu is MPU9250, lsm is LSM9DS1.
 For print help:
 ./AccelGyroMag -h
 */
+
+#include <Common/Ublox.h>
 #include <Common/MS5611.h>
 #include <string>
 #include <stdio.h>
@@ -34,6 +36,7 @@ For print help:
 #define G_SI 9.80665
 #define PI   3.14159
 
+using namespace std;
 
 std::unique_ptr <InertialSensor> get_inertial_sensor( std::string sensor_name)
 {
@@ -91,10 +94,15 @@ std::string get_sensor_name(int argc, char *argv[])
 //=============================================================================
 int main(int argc, char *argv[])
 {
+
+	if (check_apm()) {
+	        return 1;
+	    }
+
 	MS5611 barometer;
-    if (check_apm()) {
-        return 1;
-    }
+	std::vector<double> pos_data;
+	Ublox gps;
+
     auto sensor_name = get_sensor_name(argc, argv);
     if (sensor_name.empty())
         return EXIT_FAILURE;
@@ -118,6 +126,14 @@ int main(int argc, char *argv[])
     float mx, my, mz;
 //-------------------------------------------------------------------------
 
+
+    if(gps.testConnection())
+        {
+            printf("Ublox test OK\n");
+            if (!gps.configureSolutionRate(1000))
+            {
+                printf("Setting new rate: FAILED\n");
+            }
     while(1) {
 
         sensor->update();
@@ -143,7 +159,79 @@ int main(int argc, char *argv[])
                 barometer.getTemperature(), barometer.getPressure());
 
 
+        if (gps.decodeSingleMessage(Ublox::NAV_POSLLH, pos_data) == 1)
+                   {
+                       // after desired message is successfully decoded, we can use the information stored in pos_data vector
+                       // right here, or we can do something with it from inside decodeSingleMessage() function(see ublox.h).
+                       // the way, data is stored in pos_data vector is specified in decodeMessage() function of class UBXParser(see ublox.h)
+                       printf("GPS Millisecond Time of Week: %.0lf s\n", pos_data[0]/1000);
+                       printf("Longitude: %lf\n", pos_data[1]/10000000);
+                       printf("Latitude: %lf\n", pos_data[2]/10000000);
+                       printf("Height above Ellipsoid: %.3lf m\n", pos_data[3]/1000);
+                       printf("Height above mean sea level: %.3lf m\n", pos_data[4]/1000);
+                       printf("Horizontal Accuracy Estateimate: %.3lf m\n", pos_data[5]/1000);
+                       printf("Vertical Accuracy Estateimate: %.3lf m\n", pos_data[6]/1000);
 
-       usleep(1000000);
-    }
-}
+                   } else {
+                       // printf("Message not captured\n");
+                       // use this to see, how often you get the right messages
+                       // to increase the frequency you can turn off the undesired messages or tweak ublox settings
+                       // to increase internal receiver frequency
+                   }
+
+                   if (gps.decodeSingleMessage(Ublox::NAV_STATUS, pos_data) == 1)
+                   {
+                       printf("Current GPS status:\n");
+                       printf("gpsFixOk: %d\n", ((int)pos_data[1] & 0x01));
+
+                       printf("gps Fix status: ");
+                       switch((int)pos_data[0]){
+                           case 0x00:
+                               printf("no fix\n");
+                               break;
+
+                           case 0x01:
+                               printf("dead reckoning only\n");
+                               break;
+
+                           case 0x02:
+                               printf("2D-fix\n");
+                               break;
+
+                           case 0x03:
+                               printf("3D-fix\n");
+                               break;
+
+                           case 0x04:
+                               printf("GPS + dead reckoning combined\n");
+                               break;
+
+                           case 0x05:
+                               printf("Time only fix\n");
+                               break;
+
+                           default:
+                               printf("Reserved value. Current state unknown\n");
+                               break;
+
+                       }
+
+                       printf("\n");
+
+                   } else {
+                       // printf("Status Message not captured\n");
+                   }
+
+
+                   usleep(500000);
+               }
+
+           } else {
+
+               printf("Ublox test not passed\nAbort program!\n");
+
+           }
+
+           return 0;
+       }
+
