d957657e21aa84b4c7a4cb9d9d177b0b895512b5
iago silvestre <iagosilvestrehp@hotmail.com>
2018-09-21 12:50:13 -0300

    removed min max prints

--- C++/Examples/resposta/resposta.cpp
+++ C++/Examples/resposta/resposta.cpp
@@ -316,10 +316,10 @@ int main(int argc, char *argv[])
                     printf("--------------------------------------------------------------------------------------------------\n");
                    } else {
                 	   printf("Numero da leitura: %lu \n", count);
-                	   printf("Duracao minima microsegundos da leitura dos sensores: %lu \n", min);
+                	  // printf("Duracao minima microsegundos da leitura dos sensores: %lu \n", min);
                 	   printf("Duracao em microsegundos da leitura dos sensores: %lu \n", dtlong);
-                	   printf("Duracao media em microsegundos da leitura dos sensores: %lu \n", media);
-                	   printf("Duracao maxima microsegundos da leitura dos sensores: %lu \n", max);
+                	   //printf("Duracao media em microsegundos da leitura dos sensores: %lu \n", media);
+                	   //printf("Duracao maxima microsegundos da leitura dos sensores: %lu \n", max);
                 	   time_t rawtime;
                 	   struct tm * timeinfo;
 
