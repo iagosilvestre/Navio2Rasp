109df9f4aab22b8e26a730794b0cae9850de8abd
iago silvestre <iagosilvestrehp@hotmail.com>
2018-09-28 11:06:23 -0300

    try again

--- C++/Examples/TesteMultiTarefa/testeMulti.cpp
+++ C++/Examples/TesteMultiTarefa/testeMulti.cpp
@@ -92,7 +92,7 @@ void * acquireBarometerData(void * barom)
 		timersub(&t1, &t0, &dt);
         //gettimeofday(&baro2,NULL);
         //dtBaro=(1000000 * baro2.tv_sec + baro2.tv_nsec)-1000000 * baro1.tv_sec - baro1.tv_nsec-20000;
-        printf("Duracao atual em microsegundos da leitura dos sensores: %d \n", dt.tv_usec);
+        //printf("Duracao atual em microsegundos da leitura dos sensores: %d \n", dt.tv_usec);
         baroData.push_back(dt.tv_usec);
 
         //usleep(5000);
@@ -321,7 +321,7 @@ int main(int argc, char *argv[])
 	for (std::vector<int>::iterator it = baroData.begin() ; it != baroData.end(); ++it){
 		auxCount++;
 		FILE *fBaro = fopen("barometer.txt", "a");
-		fprintf(fBaro, "%d;%lu\n",auxCount,*it);
+		fprintf(fBaro, "%d;%d\n",auxCount,*it);
 		fclose(fBaro);
 	}
 
