2647f270a952d5acac3cafdf88f6db5158fcb5eb
iago silvestre <iagosilvestrehp@hotmail.com>
2018-09-28 12:04:45 -0300

    teste

--- C++/Examples/TesteMultiTarefa/testeMulti.cpp
+++ C++/Examples/TesteMultiTarefa/testeMulti.cpp
@@ -93,7 +93,7 @@ void * acquireBarometerData(void * barom)
         //gettimeofday(&baro2,NULL);
         //dtBaro=(1000000 * baro2.tv_sec + baro2.tv_nsec)-1000000 * baro1.tv_sec - baro1.tv_nsec-20000;
         //printf("Duracao atual em microsegundos da leitura dos sensores: %d \n", dt.tv_usec);
-        baroData.push_back(dt.tv_usec);
+        baroData.push_back(dt.tv_usec-20000);
 
         //usleep(5000);
     }
